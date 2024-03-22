4dc3b6275d53f7bc9a4bf2b616312bdb402beaa4
Nitin-bhatt46 <122802161+Nitin-bhatt46@users.noreply.github.com>
2024-03-22 16:36:24 +0530

    Create v4_Q2.py

---
+++ v4_Q2.py
@@ -0,0 +1,7 @@
+
+cost_of_each_potato_chip = 1.49
+no_of_potato_Chip_brought = 9
+amount_paid_to_software = 20
+
+paid = amount_paid_to_software - cost_of_each_potato_chip*no_of_potato_Chip_brought
+print("How many dollar is the shopkeeper going to give you back = ", paid)
