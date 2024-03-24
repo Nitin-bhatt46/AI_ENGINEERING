833a98bcd74c83f8b73018ba529e168a6ca2f549
Nitin-bhatt46 <122802161+Nitin-bhatt46@users.noreply.github.com>
2024-03-24 17:01:32 +0530

    Create v5_Q1.py

---
+++ v5_Q1.py
@@ -0,0 +1,10 @@
+street ="hanuman street"
+city = "Khatima"
+country ="India"
+# using concatenate +
+address1 = street +"\n"+city+"\n"+country
+print(address1)
+
+# using f string.
+address2= f'{street}\n{city}\n{country}'
+print(address2)
